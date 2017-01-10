48edf72ae4a95b2bba19378385a102dae72eb723
DEBY <debydestyananda5c@gmail.com>
2017-01-10 20:50:59 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -24,6 +24,8 @@ import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter
 {
 
+    public static final int REQUEST_CODE_EDIT = 99;
+    int itemPos;
     public static final int REQUEST_CODE_ADD = 88;
     public static final String HOTEL = "hotel";
     ArrayList<Hotel> mList = new ArrayList<>();
@@ -68,6 +70,14 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         {
             Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
             mList.add(hotel);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK)
+        {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.remove(itemPos);
+            mList.add(itemPos,hotel);
             mAdapter.notifyDataSetChanged();
         }
     }
@@ -130,5 +140,29 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(HOTEL, mList.get(pos));
         startActivity(intent);
+    }
+
+    @Override
+    public void doEdit(int pos)
+    {
+        itemPos = pos;
+        Intent intent = new Intent(this, InputActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
+        startActivityForResult(intent, REQUEST_CODE_EDIT);
+    }
+
+    @Override
+    public void doDelete(int pos) {
+
+    }
+
+    @Override
+    public void doFav(int Pos) {
+
+    }
+
+    @Override
+    public void doShare(int Pos) {
+
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,6 +33,10 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
     public interface IHotelAdapter
     {
         void doClick(int pos);
+        void doEdit(int pos);
+        void doDelete(int pos);
+        void doFav(int Pos);
+        void doShare(int Pos);
     }
 
     public HotelAdapter(Context context, ArrayList<Hotel> hotelList)
@@ -68,10 +74,13 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
     public class ViewHolder extends RecyclerView.ViewHolder
     {
 
-
         ImageView ivfoto;
         TextView tvjudul;
         TextView tvdeskripsi;
+        Button bEdit;
+        Button bDelete;
+        ImageButton ibFav;
+        ImageButton ibShare;
 
         public ViewHolder(View itemView)
         {
@@ -79,6 +88,10 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
             ivfoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvjudul = (TextView) itemView.findViewById(R.id.textViewJudul);
             tvdeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+            bEdit = (Button) itemView.findViewById(R.id.buttonEdit);
+            bDelete = (Button) itemView.findViewById(R.id.buttonDelete);
+            ibFav = (ImageButton) itemView.findViewById(R.id.buttonFavorite);
+            ibShare = (ImageButton) itemView.findViewById(R.id.buttonShare);
 
             itemView.setOnClickListener(new View.OnClickListener()
             {
@@ -88,6 +101,42 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
                     mIHotelAdapter.doClick(getAdapterPosition());
                 }
 
+            });
+
+            bEdit.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doEdit(getAdapterPosition());
+                }
+            });
+
+            bDelete.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doDelete(getAdapterPosition());
+                }
+            });
+
+            ibFav.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doFav(getAdapterPosition());
+                }
+            });
+
+            ibShare.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doShare(getAdapterPosition());
+                }
             });
 
         }
